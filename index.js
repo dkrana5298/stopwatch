a521f26ac4b7aa7dab357ae73d9601c508a0b5cb
Devendra Kumar <85422818+dkrana5298@users.noreply.github.com>
2021-12-09 12:46:43 +0530

    file added

---
+++ index.js
@@ -0,0 +1,89 @@
+// fetching all the field
+var hourCount=document.getElementById('hour');
+var minuteCount=document.getElementById('minute')
+let secondCount = document.getElementById('second');
+// fetching all the button
+var start=document.getElementById('start');
+var pausebtn=document.getElementById('pause');
+var stop=document.getElementById('stop');
+// initializing all the variable
+var interval;
+var second=00;
+var minute=00;
+var hour=00;
+// function on start button.
+start.onclick=function (){
+    console.log("clicked");
+    stop.style.display= "block";
+    stop.style.display= "inline";
+    pausebtn.innerHTML= "pause"
+ clearInterval(interval);
+ interval=setInterval(startTimer, 1000); //this function must be remember for interval purpose.
+}
+//function on pause 
+pausebtn.onclick=function (){
+   clearInterval(interval);
+   console.log("pause clicked")
+   pausebtn.innerHTML="continue";
+   Pause();
+}
+
+//function on stop
+stop.onclick=function(){
+   clearInterval(interval);
+   clearFields();
+}
+function startTimer(){
+   second++;
+   if(second < 10){
+      secondCount.innerHTML= "0" + second;
+   }
+    if(second >= 10 ){
+      secondCount.innerHTML= second;
+   }
+   if(second > 60) {
+       minute++;
+       minuteCount.innerHTML="0" + minute;
+       second=0;
+       secondCount.innerHTML= "0" + second;
+   }
+   if(minute > 9){
+       minuteCount.innerHTML=minute;
+   }
+   if(minute > 60){
+      hour++;
+      hourCount.innerHTML="0" + hour;
+      minute=0;
+      minuteCount.innerHTML=minute;
+   }
+   if(hour > 9){
+      hourCount.innerHTML= hour;
+   }
+}
+//for pausing 
+function Pause() {
+   if(second > 9){
+      secondCount.innerText = second;
+   }else {
+      secondCount.innerText ="0" + second;
+   }
+   if( minute > 9){
+      minuteCount.innerText = minute;
+   }else {
+      minuteCount.innerText ="0" + minute;
+   } 
+   if(  hour > 9){
+      hourCount.innerText = hour;
+   }else {
+      hourCount.innerText ="0" + hour;
+   }
+}
+// for clearing all the field
+function clearFields() {
+   hour =0;
+   hourCount.innerHTML="0" + hour;
+   minute = 0;
+   minuteCount.innerHTML="0" + minute;
+   second = 0;
+   secondCount.innerHTML="0" + second;
+}
